d747ea2003beed2e11ccdd9ffa6bbc843291878e
Lighting.pls <148458819+githubaccountadmin@users.noreply.github.com>
2024-04-04 07:00:43 +1300

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+function openPopup() {
+  console.log("Opening popup");
+  // Your code for opening the popup
+}
+
+function closePopup() {
+  console.log('Closing popup...');
+  // Your code for closing the popup
+}
+
+function handleButtonClick() {
+  console.log("HandleButtonClick function called.");
+  console.log("Continue button clicked!");
+  closePopup();
+}
